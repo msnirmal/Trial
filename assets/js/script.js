e209e5d058ffd115f55c1e332104ae354a8cc9db
Nirmal Subramani <kumar.n.subramani@gmail.com>
2023-06-03 20:11:36 +0000

    Trail and error

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,72 +1,29 @@
 const quizData = [
     {
-        question: "What is a group of lions called?",
-        a: "A flock",
-        b: "A pride",
-        c: "A herd",
-        correct: "b",
+    question: "What is the Capital City of india?",
+    value: "Delhi"
     },
     {
-        question: "Which bird is sometimes called a flying rat?",
-        a: "A sparrow",
-        b: "A pigeon",
-        c: "A gull",
-        correct: "b",
-    },
-    {
-        question: "Which animal recognizes itself in the mirror?",
-        a: "A cat",
-        b: "A horse",
-        c: "A chimpanzee",
-        correct: "c",
-    },
-    
-];
-const quiz= document.getElementById('quiz')
-const answerEls = document.querySelectorAll('.answer')
-const questionEl = document.getElementById('question')
-const a_text = document.getElementById('a_text')
-const b_text = document.getElementById('b_text')
-const c_text = document.getElementById('c_text')
-const submitBtn = document.getElementById('submit')
-let currentQuiz = 0
-let score = 0
-loadQuiz()
-function loadQuiz() {
-    deselectAnswers()
-    const currentQuizData = quizData[currentQuiz]
-    questionEl.innerText = currentQuizData.question
-    a_text.innerText = currentQuizData.a
-    b_text.innerText = currentQuizData.b
-    c_text.innerText = currentQuizData.c
-    
-}
-function deselectAnswers() {
-    answerEls.forEach(answerEl => answerEl.checked = false)
-}
-function getSelected() {
-    let answer
-    answerEls.forEach(answerEl => {
-        if(answerEl.checked) {
-            answer = answerEl.id
-        }
-    })
-    return answer
-}
-submitBtn.addEventListener('click', () => {
-    const answer = getSelected()
-    if(answer) {
-       if(answer === quizData[currentQuiz].correct) {
-           score++
-       }
-       currentQuiz++
-       if(currentQuiz < quizData.length) {
-           loadQuiz()
-       } else {
-           quiz.innerHTML = `
-           <h2>You answered ${score}/${quizData.length} questions correctly</h2>
-           <button onclick="location.reload()">Reload</button>
-           `
-       }
+    question: "What is the color of sky?",
+    value: "Blue"
     }
-})
+]
+
+const quizContainer = document.getElementById ('container')
+const questions = document.getElementById('question')
+const answers = document.getElementById ('answer')
+
+let currentIndex = 0
+let score = 0
+showQuiz()
+
+function showQuiz() {
+   questions.innerText= quizData[currentIndex].question
+}
+
+function userAnswer() {
+    if (answers == quizData[currentIndex].value){
+        alert ('Correct')
+        showQuiz()
+}
+ }
