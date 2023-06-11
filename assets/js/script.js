ba654fb20647a516e6a801740e44ba0652d1402b
Nirmal Subramani <kumar.n.subramani@gmail.com>
2023-06-11 17:45:34 +0000

    Made changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
 const qImages = [
-    {
-    questionimage: "assets/images/imageone.jpg",
-    correct: "Elephant"
-    },
-    {
-    questionimage: "assets/images/imagetwo.jpg", 
-    correct: "Lion"
-    },
-    {
-    questionimage: "assets/images/imagethree.jpg",
-    correct: "Tiger"
-    }
-    ]
+  {
+  questionimage: "assets/images/imageone.jpg",
+  correct: "Elephant"
+  },
+  {
+  questionimage: "assets/images/imagetwo.jpg", 
+  correct: "Lion"
+  },
+  {
+  questionimage: "assets/images/imagethree.jpg",
+  correct: "Tiger"
+  }
+  ]
 
 // Define variables for the game
 let currentIndex = 0;
@@ -19,27 +19,29 @@ let score = 0;
 
 // Display the current riddle and reset the input field
 function showquiz() {
-  document.getElementById("quiz-image").src = qImages[currentIndex].questionimage;
-  document.getElementById("guess").value = "";
-  }
+document.getElementById("quiz-image").src = qImages[currentIndex].questionimage;
+document.getElementById("guess").value = "";
+}
 
 // Check the user's answer and update the score and hint
 function checkAnswer() {
-  const userAnswer = document.getElementById("guess").value.toLowerCase();
-  const correctAnswer = qImages[currentIndex].correct.toLowerCase();
-  if (userAnswer === correctAnswer) {
-    score++;
-    document.getElementById("score").textContent = `Score: ${score}`;
-    document.getElementById("answer-result").textContent = "Correct!";
-    currentIndex++;
-    if (currentIndex === qImages.length) {
-      alert("You have solved all the questions!");
-    } else {
-      showquiz();
-    }
+const userAnswer = document.getElementById("guess").value.toLowerCase();
+const correctAnswer = qImages[currentIndex].correct.toLowerCase();
+if (userAnswer === correctAnswer) {
+  score++;
+  document.getElementById("score").textContent = `Score: ${score}/${qImages.length}`;
+  document.getElementById("answer-result").textContent = "That was correct!";
+  currentIndex++;
+  if (currentIndex === qImages.length) {
+    alert("End of Game, refresh page to restart");
   } else {
-    document.getElementById("answer-result").textContent = "Incorrect.";
+    showquiz();
   }
+} else {
+  document.getElementById("answer-result").textContent = "Incorrect.";
+  currentIndex++
+  showquiz();
+}
 }
 
 // Initialize the game
